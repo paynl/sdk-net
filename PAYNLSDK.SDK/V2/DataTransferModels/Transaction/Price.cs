582dcd3d4020f6ba1d5e2dd697abbd4e1bce65c1
SWSSolutions <info@sws-solutions.eu>
2025-06-20 10:39:14 +0200

    mismatch with API

--- PAYNLSDK.SDK/V2/DataTransferModels/Transaction/Price.cs
+++ PAYNLSDK.SDK/V2/DataTransferModels/Transaction/Price.cs
@@ -4,12 +4,6 @@ namespace PayNlSdk.Sdk.V2.DataTransferModels.Transaction;
 
 public class Price
 {
-    [JsonPropertyName("value")]
-    public int? Value { get; set; }
-
-    [JsonPropertyName("quantity")]
-    public decimal? Quantity { get; set; }
-
-    [JsonPropertyName("vatCode")]
-    public string? VatCode { get; set; }
+	[JsonPropertyName("value")]
+	public int? Value { get; set; }
 }
--- PAYNLSDK.SDK/V2/DataTransferModels/Transaction/Product.cs
+++ PAYNLSDK.SDK/V2/DataTransferModels/Transaction/Product.cs
@@ -4,15 +4,21 @@ namespace PayNlSdk.Sdk.V2.DataTransferModels.Transaction;
 
 public class Product
 {
-    [JsonPropertyName("value")]
-    public string? Id { get; set; }
+	[JsonPropertyName("id")]
+	public string? Id { get; set; }
 
-    [JsonPropertyName("description")]
-    public string? Description { get; set; }
+	[JsonPropertyName("description")]
+	public string? Description { get; set; }
 
-    [JsonPropertyName("type")]
-    public string? Type { get; set; }
+	[JsonPropertyName("type")]
+	public string? Type { get; set; }
 
-    [JsonPropertyName("price")]
-    public Price? Price { get; set; }
+	[JsonPropertyName("price")]
+	public Price? Price { get; set; }
+
+	[JsonPropertyName("quantity")]
+	public decimal? Quantity { get; set; }
+
+	[JsonPropertyName("vatCode")]
+	public string? VatCode { get; set; }
 }
